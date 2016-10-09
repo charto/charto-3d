5bdb888f5edf6723aecbbeaa7c219b75fffb5258
jjrv <juha@busfaster.com>
2016-10-09 08:06:42 +0300

    Add inverse of perspective projection matrix.

--- src/TransMatrix.ts
+++ src/TransMatrix.ts
@@ -148,6 +148,35 @@ export class TransMatrix {
 		return(matrix);
 	}
 
+	multipliedFullyBy(other: TransMatrix) {
+		const matrix = new TransMatrix();
+		const m = matrix.data;
+		const a = this.data;
+		const b = other.data;
+
+		m[ 0] = a[ 0]*b[ 0] + a[ 4]*b[ 1] + a[ 8]*b[ 2] + a[12]*b[ 3];
+		m[ 1] = a[ 1]*b[ 0] + a[ 5]*b[ 1] + a[ 9]*b[ 2] + a[13]*b[ 3];
+		m[ 2] = a[ 2]*b[ 0] + a[ 6]*b[ 1] + a[10]*b[ 2] + a[14]*b[ 3];
+		m[ 3] = a[ 3]*b[ 0] + a[ 7]*b[ 1] + a[11]*b[ 2] + a[15]*b[ 3];
+
+		m[ 4] = a[ 0]*b[ 4] + a[ 4]*b[ 5] + a[ 8]*b[ 6] + a[ 12]*b[ 7];
+		m[ 5] = a[ 1]*b[ 4] + a[ 5]*b[ 5] + a[ 9]*b[ 6] + a[ 13]*b[ 7];
+		m[ 6] = a[ 2]*b[ 4] + a[ 6]*b[ 5] + a[10]*b[ 6] + a[ 14]*b[ 7];
+		m[ 7] = a[ 3]*b[ 4] + a[ 7]*b[ 5] + a[11]*b[ 6] + a[ 15]*b[ 7];
+
+		m[ 8] = a[ 0]*b[ 8] + a[ 4]*b[ 9] + a[ 8]*b[10] + a[12]*b[11];
+		m[ 9] = a[ 1]*b[ 8] + a[ 5]*b[ 9] + a[ 9]*b[10] + a[13]*b[11];
+		m[10] = a[ 2]*b[ 8] + a[ 6]*b[ 9] + a[10]*b[10] + a[14]*b[11];
+		m[11] = a[ 3]*b[ 8] + a[ 7]*b[ 9] + a[11]*b[10] + a[15]*b[11];
+
+		m[12] = a[ 0]*b[12] + a[ 4]*b[13] + a[ 8]*b[14] + a[12]*b[15];
+		m[13] = a[ 1]*b[12] + a[ 5]*b[13] + a[ 9]*b[14] + a[13]*b[15];
+		m[14] = a[ 2]*b[12] + a[ 6]*b[13] + a[10]*b[14] + a[14]*b[15];
+		m[15] = a[ 3]*b[12] + a[ 7]*b[13] + a[11]*b[14] + a[15]*b[15];
+
+		return(matrix);
+	}
+
 	/** Format nicely for debugging. */
 
 	toString() {
@@ -248,11 +277,35 @@ export class TransMatrix {
 
 		m[0]  =         2*zmin / (xmax - xmin);
 		m[5]  =         2*zmin / (ymax - ymin);
-		m[8]  = -(xmax + xmin) / (xmax - xmin);
-		m[9]  = -(ymax + ymin) / (ymax - ymin);
+		// m[8]=-(xmax + xmin) / (xmax - xmin);
+		// m[9]=-(ymax + ymin) / (ymax - ymin);
 		m[10] =  (zmax + zmin) / (zmax - zmin);
 		m[14] = -2*zmax*zmin   / (zmax - zmin);
 		m[11] =  1;
+
+		return(matrix);
+	}
+
+	static makeInversePerspective(camera: Camera) {
+		const zmin = camera.near;
+		const zmax = camera.far;
+		const ymax = zmin * camera.ySlope;
+		const ymin = -ymax;
+		const xmin = ymin * camera.aspect;
+		const xmax = ymax * camera.aspect;
+
+		const matrix = new TransMatrix();
+		const m = matrix.data;
+
+		m[0]  =  (xmax - xmin) / (2*zmin);
+		m[5]  =  (ymax - ymin) / (2*zmin);
+		// m[12]=(xmax + xmin) / (2*zmin);
+		// m[13]=(ymax + ymin) / (2*zmin);
+		m[11] = -(zmax - zmin) / (2*zmax*zmin);
+		m[15] =  (zmax + zmin) / (2*zmax*zmin);
+		m[14] =  1;
+		// Also divide m[14] and m[15] here by contents of m[11] in
+		// makePerspective if it's nonzero
 
 		return(matrix);
 	}
